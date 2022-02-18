e21a1ab77fe54ca4910f95a3390854877299d0e0
jules chevrant breton <jules.chevrant@hotmail.fr>
2022-02-18 17:56:33 +0100

    added validations and error messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,7 +12,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(params[:id])
+    @article = Article.new(article_params)
 
     if @article.save
       redirect_to @article
